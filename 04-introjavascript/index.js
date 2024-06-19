b5be8b7dedbb93fe1657dc24fe8453f08585b32f
VictorIgnacio <victorignaciotoledo@gmail.com>
2024-06-18 21:31:10 -0300

    Intro a JS

---
+++ 04-introjavascript/index.js
@@ -0,0 +1,20 @@
+// estos son los comentarios en una sola linea en javascript
+
+/*
+comentario
+de
+varias
+lineas
+en JS
+*/
+
+//let hola = "pepe"
+
+//variables: espacios de memoria para almacenar datos
+//1.var
+// palabras reservadas: son utilizaddas solo para definiciones del lenguaje de programacion
+var nombre;
+var email
+var edad
+var pais
+
