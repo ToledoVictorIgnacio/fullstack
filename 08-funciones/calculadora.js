8b716d151c2ab40cac367db4efd933e2fc1102c2
VictorIgnacio <victorignaciotoledo@gmail.com>
2024-08-06 21:26:38 -0300

    clase math

--- 08-funciones/calculadora.js
+++ 08-funciones/calculadora.js
@@ -1,12 +1,45 @@
 const sumar = () =>{
-
     let num1= parseInt(prompt("Ingresa el primer numero"))
+
+    console.log(typeof num1);
+
+    if (nombre == typeof String) {
+        todo bien        
+    }else{
+        el nombre tiene que ser en letras
+    }
+    
+    if (num1 == "" || isNaN(num1) || num1 == null) {
+        alert("Inserte un numero")
+        return;
+    }
+
+
     let num2= parseInt(prompt("Ingresa el segundo numero"))
+    if (num2 == "" || isNaN(num2) || num2 == null) {
+        alert("Inserte un numero")
+        return;
+    }
 
     let resultado = num1 + num2
 
     alert("El resultado de la suma es: " + resultado)
 }
+
+console.log(Math.PI);
+
+
+
+console.log(Math.random() + 100);
+console.log(Math.random() + 100);
+console.log(Math.random() + 100);
+console.log(Math.random() + 100);
+console.log(Math.random() + 100);
+console.log(Math.random() + 100);
+
+
+
+
 
 const restar = () =>{
 
@@ -36,4 +69,22 @@ const dividir = () =>{
     let resultado = num1 / num2
 
     alert("El resultado de la division es: " + resultado)
+}
+
+
+
+
+
+
+
+
+const contador = () =>{
+    console.log("Presionaste restar");
+    console.log(window);
+    
+    
+} 
+
+function doble() {
+    alert("Doble click en el elemento")
 }
