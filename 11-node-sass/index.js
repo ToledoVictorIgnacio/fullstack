90098378f387f8e2fc1e070cb68f793a37c636f7
VictorIgnacio <victorignaciotoledo@gmail.com>
2024-08-20 21:31:01 -0300

    intro a sass

---
+++ 11-node-sass/index.js
@@ -0,0 +1,15 @@
+//Con los modulos de node, no necesitamos instalar nada
+//Estamos utilizando un modulo nativo de Node.js
+//os es el modulo de Node.js que nos permite trabajar con l sistema oprativo
+
+//Importamos el modulo os de Node.js
+
+const os = require('os');
+
+
+
+
+
+console.log(os.cpus());
+console.log(os.platform());
+console.log(os.version());
